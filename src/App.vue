92c16b7cac2d2953a23f917c263a3e6b1d9d32e1
AlessioDPT <alessio.recos@gmail.com>
2021-07-06 16:43:59 +0200

    ADD- scss color DC / data in Header

--- src/App.vue
+++ src/App.vue
@@ -38,4 +38,5 @@ export default {
 </script>
 
 <style lang="scss">
+    @import './components/style/Commons.scss';
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,86 @@
 <template>
   <header>
-      ciao
+      <div class="box-header">
+
+          <img src="@/assets/img/dc-logo.png" alt="DCLogo">
+
+          <nav>
+              <ul>
+                  <li><a href="#"></a></li>
+              </ul>
+          </nav>
+
+
+      </div>
+
   </header>
 </template>
 
 <script>
 export default {
+  name: 'Header',
+  data() {
+      return [
+          {
+              select: "CHARACTERS",
+              firstOpt: "Batman",
+              secondOpt:"joker"
+          },
+          {
+              select: "COMICS",
+              firstOpt: "Uscita 1",
+              secondOpt:"Uscita 2"
+          },
+          {
+              select: "MOVIES",
+              firstOpt: "Arkam Asilium",
+              secondOpt:"Suicide Squad"
+          },
+          {
+              select: "TV",
+              firstOpt: "Batman TheSeries",
+              secondOpt:"joker L'INCORREGIBILE"
+          },
+          {
+              select: "COLLECTIBLES",
+              firstOpt: "ACTION-FIGURES Batman",
+              secondOpt:"ACTION-FIGURES joker"
+          },
+          {
+              select: "VIDEOS",
+              firstOpt: "BACKSTAGE",
+              secondOpt:"Meet THE DC STUDIOS"
+          },
+          {
+              select: "FANS",
+              firstOpt: "OUR SOCIAL",
+              secondOpt:"FanBase"
+          },
+          {
+              select: "NEWS",
+              firstOpt: "LAST NEWS",
+              secondOpt:"Weekly Highlights"
+          },
+          {
+              select: "SHOP",
+              firstOpt: "Merch",
+              secondOpt:"Offerts"
+          }
+      ]
+  }
 
 }
 // importazione file scss
-import '../components/style/commons.scss'
+import '../components/style/Commons.scss'
+import '../components/style/BlueDC.scss'
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .box-header {
+        height: 115px;
+        background-color: red;
+        padding: 2% 17%;
+        
+    }
     
 </style>
