85600b538c22d5289136074339bfa15be491f3b1
AlessioDPT <alessio.recos@gmail.com>
2021-07-06 17:07:14 +0200

    ADD-style in COMP Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,9 @@
 
           <nav>
               <ul>
-                  <li><a href="#"></a></li>
+                  <li v-for="(item, index) in navig" :key="index">
+                      <a href="#">{{item.select}}</a>
+                    </li>
               </ul>
           </nav>
 
@@ -20,53 +22,64 @@
 export default {
   name: 'Header',
   data() {
-      return [
+      return {
+        navig: [
           {
               select: "CHARACTERS",
               firstOpt: "Batman",
-              secondOpt:"joker"
+              secondOpt:"joker",
+              status: false
           },
           {
               select: "COMICS",
               firstOpt: "Uscita 1",
-              secondOpt:"Uscita 2"
+              secondOpt:"Uscita 2",
+              status: false
           },
           {
               select: "MOVIES",
               firstOpt: "Arkam Asilium",
-              secondOpt:"Suicide Squad"
+              secondOpt:"Suicide Squad",
+              status: false
           },
           {
               select: "TV",
               firstOpt: "Batman TheSeries",
-              secondOpt:"joker L'INCORREGIBILE"
+              secondOpt:"joker L'INCORREGIBILE",
+              status: false
           },
           {
               select: "COLLECTIBLES",
               firstOpt: "ACTION-FIGURES Batman",
-              secondOpt:"ACTION-FIGURES joker"
+              secondOpt:"ACTION-FIGURES joker",
+              status: false
           },
           {
               select: "VIDEOS",
               firstOpt: "BACKSTAGE",
-              secondOpt:"Meet THE DC STUDIOS"
+              secondOpt:"Meet THE DC STUDIOS",
+              status: false
           },
           {
               select: "FANS",
               firstOpt: "OUR SOCIAL",
-              secondOpt:"FanBase"
+              secondOpt:"FanBase",
+              status: false
           },
           {
               select: "NEWS",
               firstOpt: "LAST NEWS",
-              secondOpt:"Weekly Highlights"
+              secondOpt:"Weekly Highlights",
+              status: false
           },
           {
               select: "SHOP",
               firstOpt: "Merch",
-              secondOpt:"Offerts"
+              secondOpt:"Offerts",
+              status: false
           }
-      ]
+        ]
+      }
   }
 
 }
@@ -80,7 +93,31 @@ import '../components/style/BlueDC.scss'
         height: 115px;
         background-color: red;
         padding: 2% 17%;
-        
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        img {    
+        height: 100%;
+        }
+
+        li {
+            display: inline-block;
+            margin: 0 15px;
+            cursor: pointer;
+            padding: 47px 0;
+
+            &:hover {
+                border-bottom: 3px solid black;
+                padding: 44px 0;
+
+            }
+
+            a {
+                color: rgb(68, 67, 67);
+                text-decoration: none;
+            }
+        }
     }
     
 </style>
