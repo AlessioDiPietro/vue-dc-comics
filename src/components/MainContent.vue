8d3033a9436da0af66648e0ef8cfab5b090f973b
AlessioDPT <alessio.recos@gmail.com>
2021-07-06 17:19:47 +0200

    ADD- style in  COMP MainContent

---
+++ src/components/MainContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+
+      <h2> __ Content goes  here __ </h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MainContent',
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        padding: 55px 15%;
+        background-color: black;
+        color: white;
+    }
+
+
+</style>
