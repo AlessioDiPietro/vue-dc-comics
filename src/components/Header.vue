1da7336f38aa39f8e894116004fbd89473a03dc9
AlessioDPT <alessio.recos@gmail.com>
2021-07-06 16:03:47 +0200

    ADD- scss commons style

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+      ciao
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+// importazione file scss
+import '../components/style/commons.scss'
+</script>
+
+<style>
+    
+</style>
